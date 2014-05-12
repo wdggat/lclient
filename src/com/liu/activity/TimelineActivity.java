9b6b984fee4d74b862d2cbaccf8a3d24da113f6b
wdggat <wdggat@163.com>
2014-05-13 01:02:36 +0800

    add permission

--- src/com/liu/activity/TimelineActivity.java
+++ src/com/liu/activity/TimelineActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 public class TimelineActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_timeline);
 		
 	}
 }
