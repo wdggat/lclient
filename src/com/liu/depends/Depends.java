ce08319fda645bdb6c80d53df735fcd03c6a671a
刘小龙 <hzliuxiaolong@corp.netease.com>
2014-07-09 01:21:47 +0800

    daily

--- src/com/liu/depends/Depends.java
+++ src/com/liu/depends/Depends.java
@@ -22,8 +22,8 @@ public class Depends {
         // 您可以用自定义常量等其它方式实现，来替换参数中的Utils.getMetaValue(PushDemoActivity.this,
         // "api_key")
         // 通过share preference实现的绑定标志开关，如果已经成功绑定，就取消这次绑定
-//        if (!Utils.hasBind(context.getApplicationContext())) {
-		if(true){
+        if (!Utils.hasBind(context.getApplicationContext())) {
+//		if(true){
             PushManager.startWork(context.getApplicationContext(),
                     PushConstants.LOGIN_TYPE_API_KEY,
                     Utils.getMetaValue(context, "api_key"));
