9e95b7d3e1cdfc2f781c040f5af8c6c4147c5328
刘小龙 <hzliuxiaolong@corp.netease.com>
2014-08-19 18:00:22 +0800

    add ListCache

---
+++ src/com/liu/helper/ListCache.java
@@ -0,0 +1,20 @@
+package com.liu.helper;
+
+import org.apache.commons.collections.list.TreeList;
+
+public class ListCache<E extends Comparable<E>> {
+	private TreeList items;
+	
+	private ListCache() {
+		items = new TreeList(); 
+	}
+	
+	public void push(E item) {
+		
+	}
+	
+    public void get(int position) {
+		
+	}
+    
+}
---
+++ src_test/com/liu/activity/ListCacheTest.java
@@ -0,0 +1,5 @@
+package com.liu.activity;
+
+public class ListCacheTest {
+	
+}
